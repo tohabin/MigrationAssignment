251b37e122406726f1f71fb74a86aa615bf94104
Toha <39374275+tohabin@users.noreply.github.com>
2023-12-10 01:49:29 +0600

    first commit

---
+++ database/migrations/2023_12_09_174836_create_products_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->decimal('amount', $precision = 8, $scale = 2);
+            $table->text('description');
+            $table->integer('quantity')->nullable($value = true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
